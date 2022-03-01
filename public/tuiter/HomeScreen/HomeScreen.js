6d702e0ce96ef3867f40bcf2d3891defdf973c0f
spicerk <kristispicer124@gmail.com>
2022-02-28 21:59:43 -0500

    a5- finished challenge

--- public/tuiter/HomeScreen/HomeScreen.js
+++ public/tuiter/HomeScreen/HomeScreen.js
@@ -1,18 +1,18 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
-import WhoToFollowList from "../WhoToFollowList/index.js";
-import ExploreComponent from "./ExploreComponent.js";
+import PostSummaryList from "../PostSummaryList/index.js";
+import HomeComponent from "./HomeComponent.js";
 
 (function ($) {
-    $('#wd-explore').append(`
+    $('#wd-home').append(`
         <div class="row mt-2 flex-nowrap wd-main-content">
             <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-                ${NavigationSidebar("explore")}
+                ${NavigationSidebar("home")}
             </div>
             <div class="col-sm-11 col-md-11 col-lg-7 col-xl-6 col-xxl-6">
-                ${ExploreComponent()}
+                ${HomeComponent()}
             </div>
             <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-                ${WhoToFollowList()}
+                ${PostSummaryList()}
             </div>
         </div>
     `);
